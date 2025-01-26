34dbf5c955346e50ce9a9c96257a59f87e38993d
AJ Sarama <ajsarama.cs@gmail.com>
2025-01-25 22:30:22 -0500

    gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,4 +1,4 @@
-return {
+local onedark = {
 	"navarasu/onedark.nvim",
 	lazy = false,
 	priority = 1000,
@@ -7,3 +7,25 @@ return {
 		require('onedark').load()
 	end,
 }
+
+local kanagawa = {
+	"rebelot/kanagawa.nvim",
+	lazy = false,
+	priority = 1000,
+	opts = {},
+	init = function()
+		vim.cmd("colorscheme kanagawa")
+	end,
+}
+
+local gruvbox = {
+	"ellisonleao/gruvbox.nvim",
+	lazy = false,
+	priority = 1000,
+	init = function()
+		vim.opt.background = "dark"
+		vim.cmd("colorscheme gruvbox")
+	end,
+}
+
+return { gruvbox }
